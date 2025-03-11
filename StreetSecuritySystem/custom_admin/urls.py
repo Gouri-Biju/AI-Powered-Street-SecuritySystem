4552192638f6a81f1300005ea3910ed9a94424a2
Gouri Biju <164582227+Gouri-Biju@users.noreply.github.com>
2025-03-11 14:15:30 +0530

    File Upload

--- StreetSecuritySystem/custom_admin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import admin_login, admin_dashboard, admin_logout, view_logs
-
-urlpatterns = [
-    path("login/", admin_login, name="admin_login"),
-    path("dashboard/", admin_dashboard, name="admin_dashboard"),
-    path("logout/", admin_logout, name="admin_logout"),
-    path("logs/", view_logs, name="view_logs"),
-]
